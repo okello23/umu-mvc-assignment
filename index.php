7f761e5242fc4161957b3e67ae6c0125cf750172
okello23 <benbyron24@gmail.com>
2020-05-09 22:36:57 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE htm>
+<html>
+<body>
+  <title>Lab Data Aggregator</title>
+  <h2>Monthly & Annual Laboratory Data Aggregator</h2>
+
+      <div class="panel panel-primary" style="border: 2px solid blue;">
+    <p style="color:blue">This is a classroom MVC project designed and written by <b>Benson Okello<i>(2019-m132-20010)</i></b><br>
+      basing on dummy data retrieved from A-LIS test dataset.
+      <br>
+      <br>
+      <b style="font-size:30">Issue</b><br>
+      <i>Laboratories collect alot of data on patients and their lab results but then
+      highly <br>rely on the use of manual systems to generate reports for decision making,<br>
+      accounting for testing kits and other Laboratory consummables, a process which
+      takes<br> a very long time to accomplish and often times lead to inaccurate reports.<br><br>
+      This project is a skeleton on which automation of the generation of some of these
+      very <br>much demanded reports can be built. <br>This project considered the most daunting and tideous
+      reports (Monthly and Annual reports) <br>which summarizes the total number of tests performed in a given period and<br> further aggregates them into gender.</i>
+     </p>
+  </div>
+  <br>
+      <div class="panel panel-primary" style="border: 2px solid green;">
+    <h3>Monthly Aggregated Laboratory reports of 2019 </h3>
+    <div class = "form-inline">
+    	<ul>
+    		<li><a href="jan.phtml">January 2019 Aggregated Report</a></li>
+    </ul>
+    <ul>
+    		<li><a href="mar.phtml">March 2019 Aggregated Report</a></li>
+    </ul>
+    <ul>
+    		<li><a href="apr.phtml">April 2019 Aggregated Report</a></li>
+    	</ul>
+    <ul>
+    		<li><a href="may.phtml">May 2019 Aggregated Report</a></li>
+    	</ul>
+    <ul>
+    		<li><a href="june.phtml">June 2019 Aggregated Report</a></li>
+    	</ul>
+    <ul>
+    		<li><a href="july.phtml">July 2019 Aggregated Report</a></li>
+    	</ul>
+    <ul>
+    		<li><a href="aug.phtml">August 2019 Aggregated Report</a></li>
+    	</ul>
+    <ul>
+    		<li><a href="sep.phtml">September 2019 Aggregated Report</a></li>
+    	</ul>
+    <ul>
+    		<li><a href="oct.phtml">October 2019 Aggregated Report</a></li>
+    	</ul>
+    <ul>
+    		<li><a href="nov.phtml">November 2019 Aggregated Report</a></li>
+    	</ul>
+    <ul>
+    		<li><a href="dec.phtml">December 2019 Aggregated Report</a></li>
+    	</ul>
+    </div>
+  </div>
+</body>
+<footer>
+ <img src="okello.gif" alt="Smiley face" height="150" width="150"> 
+</footer>
+</html>
